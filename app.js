56d90a7eb2d48bbf84dfdf028d1ef4db681595cf
Spencer Lyons <sjl8r8@gmail.com>
2022-06-18 21:34:36 -0500

    adding alpine js

---
+++ app.js
@@ -0,0 +1,23 @@
+const about = {
+    titleOne: "Back Story",
+    contentOne: "I grew up in Kansas City and went to the University of Missouri for college. After college, I left Kansas City for the East Coast. I landed in Philadelphia where I worked in the investment accounting industry. After 7+ years in the investment accounting industry and a run at entrepreneurship, my experiences have led me to become a software developer. I found more passion in coding than I have with any other industry. I love problem solving and coding has given me the chance to expand on those skills.",
+    titleTwo: "LauchCode Graduate",
+    contentTwo: "I graduated from LaunchCode's LC101 program this past March and just finished their LiftOff program in June. LaunchCode is a national nonprofit offering free tech education and job placement opportunities. Their goal is to bring new people from all backgrounds into the tech field and reshape the way employers think about hiring. LaunchCode is highly competitive. The amount of value and education the program offers is exceptional and I'm very thankful to get that experience. My goal is to take my knowledge and apply it to the real world.",
+    titleThree: "Ready To Work",
+    contentThree: "I'm currently seeking a role as a full time developer. My strength is with JavaScript, so I would love to work with React or Node but eager to learn other frameworks and languages. I enjoy working in small teams and in a collaborative environment where ideas and thoughts are shared freely to solve problems. My business background has given me the chance to see both sides of the equation between business and technology teams. I understand the high cost that comes with breakdown of communication between the two. I believe I can help fill those gaps and make sure challenges from both sides are communicated effectively."
+}
+
+const skills = {
+    titleOne: "Problem Solving",
+    contentOne: "I understand to solve a complex problem requires breaking the problem down, working through possible solutions and determining what is the best path forward.",
+    titleTwo: "Business Acumen",
+    contentTwo: "My experience has given me the opportunity to work with a wide range of clients, understand their needs and be able to effectively communicate back to the team.",
+    titleThree: "Coding",
+    contentThree: "In addition to LaunchCode, I have been teaching myself to code for the last nine months and have developed a strong passion for it. Major problems can be solved with a few lines of code. I enjoy working with Javascript, especially React.",
+    list: ['HTML', 'CSS', 'JavaScript', 'React', 'React Native', 'Node.js', 'Vue.js', 'Alpine.js', 'SQL', 'C#', 'PHP/Laravel' ]
+}
+
+
+
+
+window.about = about;
